9946f6107f6482cf981ddce6c595075063d6d565
Kevin Martins <kevinmmartins@gmail.com>
2017-12-19 20:45:38 -0200

    Fix the simple service order list

--- camel.module/src/main/java/com/integration/camel/module/handler/SimpleServiceOrderHandler.java
+++ camel.module/src/main/java/com/integration/camel/module/handler/SimpleServiceOrderHandler.java
@@ -1,9 +1,9 @@
 package com.integration.camel.module.handler;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
@@ -14,20 +14,21 @@ import com.integration.camel.module.to.SimpleServiceOrder;
 
 public class SimpleServiceOrderHandler {
 
-	private Map<Integer, SimpleServiceOrder> service = new HashMap<Integer, SimpleServiceOrder>();
+	private List<SimpleServiceOrder> service = new ArrayList<SimpleServiceOrder>();
 
 	public void init() {
 		add(new SimpleServiceOrder(1l, "2017-1", "Mogi Guaçu"));
+		add(new SimpleServiceOrder(2l, "2017-2", "Limeira"));
 	}
 
 	public Response add(SimpleServiceOrder so) {
-		service.put(so.getId().intValue(), so);
+		service.add(so);
 
 		return Response.created(URI.create("/personservice/person/get/" + so.getId())).build();
 	}
 
 	public SimpleServiceOrder getServiceOrderByLabel(String label) {
-		SimpleServiceOrder so = service.get(1l);
+		SimpleServiceOrder so = service.get(1);
 		if (so == null) {
 			ResponseBuilder builder = Response.status(Status.NOT_FOUND);
 			throw new WebApplicationException(builder.build());
@@ -36,13 +37,13 @@ public class SimpleServiceOrderHandler {
 		return so;
 	}
 
-	public Collection<SimpleServiceOrder> getServiceOrderList() {
+	public List<SimpleServiceOrder> getServiceOrderList() {
 		if (service == null) {
 			ResponseBuilder builder = Response.status(Status.NOT_FOUND);
 			throw new WebApplicationException(builder.build());
 		}
 
-		return service.values();
+		return service;
 	}
 
 	public Response createNewServiceOrder(SimpleServiceOrder so) {
